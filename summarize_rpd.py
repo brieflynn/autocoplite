4f4e2cf32c80957e78f6c4a85ee88c89ea28f574
brieflynn <111904006+brieflynn@users.noreply.github.com>
2024-06-18 10:07:43 -0500

    specify output filename optionally

--- summarize_rpd.py
+++ summarize_rpd.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 import argparse
 
-def export_sqlite_to_csv(database_file):
+def export_sqlite_to_csv(database_file, outname='trace_summary.csv'):
     # Connect to the SQLite database
     conn = sqlite3.connect(database_file)
     
@@ -10,11 +10,11 @@ def export_sqlite_to_csv(database_file):
         # Export top table to CSV
         top_query = "SELECT * FROM top"
         df_top = pd.read_sql_query(top_query, conn)
-        df_top.to_csv('trace_summary.csv', index=False)
+        df_top.to_csv(outname, index=False)
     
     finally:
         # Close the database connection
         conn.close()
 
 ## Example
-# export_sqlite_to_csv('trace.rpd')
+# export_sqlite_to_csv('trace.rpd', 'llama_output.csv')
